4051830aec067ca191b7157df929bac5f7c96e83
Rainbowizard67 <61212429+Rainbowizard67@users.noreply.github.com>
2023-09-16 19:44:05 -0400

    update to images

--- files/text_detection.py
+++ files/text_detection.py
@@ -10,7 +10,7 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'shentai_file.json'
 
 client = vision.ImageAnnotatorClient()
 
-image_path = 'images\download.jpg'
+image_path = 'images\Spanish-Alphabet-1.png'
 
 Text_Detection = namedtuple('Text_Detection', ('description', 'bounding_poly'))
 
