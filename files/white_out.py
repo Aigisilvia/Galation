5c2327ab2952de0bd19a46eda9af8988ba8e675b
Grayson <graykwing@gmail.com>
2023-09-16 23:27:18 -0400

    Update white_out.py

--- files/white_out.py
+++ files/white_out.py
@@ -3,9 +3,9 @@ import matplotlib.patches as patches
 from PIL import Image
 import numpy as np
 
-def white(ax,ay,width,height):
+def white(anchorX,anchorY,width,height):
 
-    x = np.array(Image.open('geek.png'), dtype=np.uint8)
+    x = np.array(Image.open('images\download.jpg'), dtype=np.uint8)
     plt.imshow(x)
   
     # Create figure and axes
@@ -15,8 +15,15 @@ def white(ax,ay,width,height):
     ax.imshow(x)
   
     # Create a Rectangle patch
-    rect = patches.Rectangle((ax, ay), width, height, linewidth=1, edgecolor='w', facecolor="w")
+    rect = patches.Rectangle((anchorX, anchorY), width, height, linewidth=1, edgecolor='w', facecolor="w")
   
     # Add the patch to the Axes
     ax.add_patch(rect)
     plt.show()
+    
+    
+        
+white(50,50,100,100)        
+        
+        
+        
