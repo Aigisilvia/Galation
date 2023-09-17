576cd6addcdf63ddb32950ed90e7eec4445e1d4e
Grayson <graykwing@gmail.com>
2023-09-16 22:38:35 -0400

    Update white_out.py

--- files/white_out.py
+++ files/white_out.py
@@ -3,19 +3,20 @@ import matplotlib.patches as patches
 from PIL import Image
 import numpy as np
 
-x = np.array(Image.open('geek.png'), dtype=np.uint8)
-plt.imshow(x)
+def white(ax,ay,width,height):
+
+    x = np.array(Image.open('geek.png'), dtype=np.uint8)
+    plt.imshow(x)
   
-# Create figure and axes
-fig, ax = plt.subplots(1)
+    # Create figure and axes
+    fig, ax = plt.subplots(1)
   
-# Display the image
-ax.imshow(x)
+    # Display the image
+    ax.imshow(x)
   
-# Create a Rectangle patch
-rect = patches.Rectangle((50, 100), 40, 30, linewidth=1,
-                         edgecolor='r', facecolor="g")
+    # Create a Rectangle patch
+    rect = patches.Rectangle((ax, ay), width, height, linewidth=1, edgecolor='w', facecolor="w")
   
-# Add the patch to the Axes
-ax.add_patch(rect)
-plt.show()
+    # Add the patch to the Axes
+    ax.add_patch(rect)
+    plt.show()
